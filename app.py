2274b008282a48ea9fd23f6a73acfba28e985c69
donvgi2001 <donvgi2001@yahoo.com>
2022-11-22 22:10:44 +0800

    try remove 16 bit audio

--- app.py
+++ app.py
@@ -257,9 +257,7 @@ def main():
     with st.spinner(f"Transcribing to FluidSynth"):
         midi_data = pretty_midi.PrettyMIDI(out_pm)
         audio_data = midi_data.fluidsynth()
-        audio_data = np.int16(
-            audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
-        )  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
+        #audio_data = np.int16(audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9)  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
 
         virtualfile = io.BytesIO()
         wavfile.write(virtualfile, 44100, audio_data)

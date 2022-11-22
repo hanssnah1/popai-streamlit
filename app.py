e351a55d917e065e31fc6d9b784f431171b011bb
donvgi2001 <donvgi2001@yahoo.com>
2022-11-22 22:19:45 +0800

    midi_data is out_pm

--- app.py
+++ app.py
@@ -255,7 +255,8 @@ def main():
     st.markdown("---")
 
     with st.spinner(f"Transcribing to FluidSynth"):
-        midi_data = pretty_midi.PrettyMIDI(out_pm)
+        #midi_data = pretty_midi.PrettyMIDI(out_pm)
+        midi_data = out_pm
         audio_data = midi_data.fluidsynth()
         audio_data = np.int16(audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9)  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
 

769dadb22ef713535ce6a9ddb5ca5e1f3a19c1b0
donvgi2001 <donvgi2001@yahoo.com>
2022-11-30 10:54:03 +0800

    try string audio data

--- app.py
+++ app.py
@@ -283,12 +283,11 @@ def main():
     st.markdown("---")
 
     with st.spinner(f"Please Wait"):
-
         midi_data = output
         audio_data = midi_data.fluidsynth()
         audio_data = np.int16(audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9)  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
         f = open("output.mid", "w")
-        f.write(audio_data)
+        f.write(str(audio_data))
         virtualfile = io.BytesIO()
         wavfile.write(virtualfile, 44100, audio_data)
 

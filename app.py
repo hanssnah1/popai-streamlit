c14aba5fb248f8c30cdcbc455550f5acbd9fd8cb
donvgi2001 <donvgi2001@yahoo.com>
2022-11-30 11:07:02 +0800

    testing .write prettymidi

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ def main():
         audio_data = midi_data.fluidsynth()
         audio_data = np.int16(audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9)  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
         f = open("output.mid", "w")
-        f.write(str(audio_data))
+        output.write('output.mid')
         virtualfile = io.BytesIO()
         wavfile.write(virtualfile, 44100, audio_data)
 
